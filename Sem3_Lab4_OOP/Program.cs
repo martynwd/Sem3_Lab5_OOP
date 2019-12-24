69282e6a45c35a60d8c595fdfa1b8f42d063c495
Martynov Daniil <dendimart@gmail.com>
2019-12-24 20:51:41 +0300

    Add local tests

--- Sem3_Lab4_OOP/Program.cs
+++ Sem3_Lab4_OOP/Program.cs
@@ -7,18 +7,18 @@ namespace Sem3_Lab4_OOP
         static void Main(string[] args)
         {
             Service service = new Service();
-            //service.CreateShop(1, "Окей");
+            service.CreateShop(1, "Окей");
             //System.IO.File.WriteAllText("Shops.csv", "Test");            
             //service.CreateShop(6, "Ашан");
             //service.ShowShops();
-            var product1 = service.CreateProduct("Шоколад 'Аленка'", 1);
+            var product1 = service.CreateProduct("Шоколад 'Аленка'", 100);
             //service.AddProduct(1, product1);
-            var product2 = service.CreateProduct("Телевизор PHILIPS", 6);
+            service.Buy(1, "Окей", 50);
             //service.AddProduct(1, product2);
             //service.AddProduct(6, service.CreateProduct("Телевизор PHILIPS", 1, (decimal)21000.00));
             //Console.WriteLine(service.FindCheapest("Шоколад 'Аленка'").Name);
             //Console.WriteLine($"Price for product: {service.Buy(1, "Шоколад 'Аленка'", 20)}");
-            Console.WriteLine(service.FindCheapestSum(product1, product2).Name);
+            //Console.WriteLine(service.FindCheapestSum(product1, product2).Name);
         }
     }
 }
